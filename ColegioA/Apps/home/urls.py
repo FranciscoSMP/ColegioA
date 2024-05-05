90fbb0292148ad396afed6a84da783b5b3a541f5
FranciscoSMP <fmoralesp2001@gmail.com>
2024-05-04 21:42:44 -0600

    clase 8

--- ColegioA/Apps/home/urls.py
+++ ColegioA/Apps/home/urls.py
@@ -21,7 +21,11 @@ from.views import *
 app_name = 'home'
 urlpatterns = [
     path('', HomeView.as_view(), name='homeapp'),
-    path('listado/', ListadoView.as_view(), name='listadoapp'),
+    path('listado/', ListarEstudiantesPView.as_view(), name='listadoapp'),
+    path('listadoA/', ListarEstudiantesAView.as_view(), name='listadoAapp'),
+    path('listadoC/', ListarComentarios.as_view(), name='listadoCapp'),
+    path('listadoArti/', ListarArticulos.as_view(), name='listadoArtiapp'),
+    path('listadoP/', ListarPublicaciones.as_view(), name='listadoPapp'),
     path('crearEstA/,', CrearEstudianteAView.as_view(), name='crearEstAapp'),
     path('crearEstP/,', CrearEstudiantePView.as_view(), name='crearEstP'),
     path('crearArt/,', CrearArticuloView.as_view(), name='crearArtapp'),
--- ColegioA/Apps/home/views.py
+++ ColegioA/Apps/home/views.py
@@ -1,6 +1,7 @@
+from django.db.models.query import QuerySet
 from django.shortcuts import render
-from django.views.generic import TemplateView, CreateView
-from .forms import EstudiantePForm, EstudianteAForm, ComentarioForm, PublicacionForm, ArticuloForm 
+from django.views.generic import TemplateView, CreateView, ListView
+from .forms import * 
 from django.urls import reverse_lazy
 
 # Create your views here.
@@ -8,30 +9,67 @@ from django.urls import reverse_lazy
 class HomeView(TemplateView):
     template_name='home.html'
 
-class ListadoView(TemplateView):
-    template_name='listado.html'
-
 class CrearEstudianteAView(CreateView):
     template_name='crearEstudianteA.html'
     form_class=EstudianteAForm
     success_url=reverse_lazy('home:listadoapp')
 
 class CrearEstudiantePView(CreateView):
-    template_name='crearEstudianteP.html'
-    form_class=EstudiantePForm
-    success_url=reverse_lazy('home:listadoapp')
+    template_name = 'CrearEstudianteP.html'
+    form_class = EstudiantePForm
+    success_url = reverse_lazy('home:listadoapp')
 
 class CrearArticuloView(CreateView):
-    template_name='CrearArticulo.html'
-    form_class=ArticuloForm
-    success_url=reverse_lazy('home:listadoapp')
+    template_name = 'CrearArticulo.html'
+    form_class = ArticuloForm
+    success_url = reverse_lazy('home:listadoapp')
 
 class CrearPublicacionView(CreateView):
-    template_name='CrearPublicacion.html'
-    form_class=PublicacionForm
-    success_url=reverse_lazy('home:listadoapp')
+    template_name = 'CrearPublicacion.html'
+    form_class = PublicacionForm
+    success_url = reverse_lazy('home:listadoapp')
 
 class CrearComentarioView(CreateView):
-    template_name='CrearComentario.html'
-    form_class=ComentarioForm
-    success_url=reverse_lazy('home:listadoapp')
+    template_name = 'CrearComentario.html'
+    form_class = ComentarioForm
+    success_url = reverse_lazy('home:listadoapp')
+
+class ListarEstudiantesPView(ListView):
+    template_name = 'listado.html'
+    model = EstudiantesPublicaciones
+    context_object_name = 'estudiantespublicaciones_list'
+
+    def get_query(self):
+        return EstudiantesPublicaciones.objects.all()
+
+class ListarEstudiantesAView(ListView):
+    template_name = 'listadoA.html'
+    model = EstudiantesAutorizaciones
+    context_object_name = 'estudiantesautorizaciones_list'
+
+    def get_query(self):
+        return EstudiantesAutorizaciones.objects.all() 
+
+class ListarComentarios(ListView):
+    template_name = 'listadoC.html'
+    model = Comentarios
+    context_object_name = 'comentarios_list'
+
+    def get_query(self):
+        return Comentarios.objects.all()
+
+class ListarArticulos(ListView):
+    template_name = 'listadoArti.html'
+    model = Articulos
+    context_object_name = 'articulos_list'
+
+    def get_query(self):
+        return Articulos.objects.all() 
+
+class ListarPublicaciones(ListView):
+    template_name = 'listadoP.html'
+    model = Publicaciones
+    context_object_name = 'publicaciones_list'
+
+    def get_query(self):
+        return Publicaciones.objects.all() 
